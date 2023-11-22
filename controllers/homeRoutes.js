b0c8a7b5e834f0106eae818e47984479d00bd2d7
Andrea <keriazes.andrea@gmail.com>
2023-11-22 14:30:49 -0500

    download dotenv

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -28,7 +28,7 @@ router.get('/', async (req, res) => {
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-      projects, 
+      blogs, 
       logged_in: req.session.logged_in 
     });
   } catch (err) {
